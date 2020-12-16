cd6aa6f15d4268598b61aaa7cca065af90f9dce9
Abdullah Khallouf <a.khallouf@outlook.com>
2020-12-16 12:03:45 +0300

    created some core native components in app.tsx

--- App.tsx
+++ App.tsx
@@ -1,13 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+  <View style={styles.container}>
       <Text>Open App.tsx to start working on your app!</Text>
+      <TextInput placeholder="type something here"/>   
       <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
+    
+      <Text style={{ fontSize: 96 }}>If you like</Text>
+    
+      <Text style={{ fontSize: 96 }}>Scrolling down</Text>
+    </ScrollView>
+    <FlatList data={[{number: 1},{number: 10}]} 
+    renderItem={({item})=><Text>{item.number}</Text>} / >
+  </View>
   );
 }
 
